3da75d3387c62ad8d043e4ddb8bbff46d4405e4c
kirpanarda <kirpanarda@gmail.com>
2021-11-04 14:16:37 +0300

    fonksiyon
    .

---
+++ fonkiyon/a/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace a_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_topla_sayi1 = new System.Windows.Forms.TextBox();
+            this.textBox_topla_sayi2 = new System.Windows.Forms.TextBox();
+            this.button_toplama = new System.Windows.Forms.Button();
+            this.button_cıkart = new System.Windows.Forms.Button();
+            this.textBox_cıkart_sayi2 = new System.Windows.Forms.TextBox();
+            this.textBox_cıkart_sayi1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_carpma = new System.Windows.Forms.Button();
+            this.textBox_carpma_sayi2 = new System.Windows.Forms.TextBox();
+            this.textBox_carpma_sayi1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button_bolme = new System.Windows.Forms.Button();
+            this.textBox_bolme_sayi2 = new System.Windows.Forms.TextBox();
+            this.textBox_bolme_sayi1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "sayi1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "sayi2";
+            // 
+            // textBox_topla_sayi1
+            // 
+            this.textBox_topla_sayi1.Location = new System.Drawing.Point(125, 41);
+            this.textBox_topla_sayi1.Name = "textBox_topla_sayi1";
+            this.textBox_topla_sayi1.Size = new System.Drawing.Size(100, 23);
+            this.textBox_topla_sayi1.TabIndex = 2;
+            // 
+            // textBox_topla_sayi2
+            // 
+            this.textBox_topla_sayi2.Location = new System.Drawing.Point(125, 83);
+            this.textBox_topla_sayi2.Name = "textBox_topla_sayi2";
+            this.textBox_topla_sayi2.Size = new System.Drawing.Size(100, 23);
+            this.textBox_topla_sayi2.TabIndex = 3;
+            this.textBox_topla_sayi2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button_toplama
+            // 
+            this.button_toplama.Location = new System.Drawing.Point(137, 112);
+            this.button_toplama.Name = "button_toplama";
+            this.button_toplama.Size = new System.Drawing.Size(75, 23);
+            this.button_toplama.TabIndex = 4;
+            this.button_toplama.Text = "toplama";
+            this.button_toplama.UseVisualStyleBackColor = true;
+            this.button_toplama.Click += new System.EventHandler(this.button_toplama_Click);
+            // 
+            // button_cıkart
+            // 
+            this.button_cıkart.Location = new System.Drawing.Point(400, 112);
+            this.button_cıkart.Name = "button_cıkart";
+            this.button_cıkart.Size = new System.Drawing.Size(75, 23);
+            this.button_cıkart.TabIndex = 9;
+            this.button_cıkart.Text = "çıkart";
+            this.button_cıkart.UseVisualStyleBackColor = true;
+            this.button_cıkart.Click += new System.EventHandler(this.button_cıkart_Click);
+            // 
+            // textBox_cıkart_sayi2
+            // 
+            this.textBox_cıkart_sayi2.Location = new System.Drawing.Point(388, 83);
+            this.textBox_cıkart_sayi2.Name = "textBox_cıkart_sayi2";
+            this.textBox_cıkart_sayi2.Size = new System.Drawing.Size(100, 23);
+            this.textBox_cıkart_sayi2.TabIndex = 8;
+            // 
+            // textBox_cıkart_sayi1
+            // 
+            this.textBox_cıkart_sayi1.Location = new System.Drawing.Point(388, 41);
+            this.textBox_cıkart_sayi1.Name = "textBox_cıkart_sayi1";
+            this.textBox_cıkart_sayi1.Size = new System.Drawing.Size(100, 23);
+            this.textBox_cıkart_sayi1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(308, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "sayi2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(308, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "sayi1";
+            // 
+            // button_carpma
+            // 
+            this.button_carpma.Location = new System.Drawing.Point(149, 257);
+            this.button_carpma.Name = "button_carpma";
+            this.button_carpma.Size = new System.Drawing.Size(75, 23);
+            this.button_carpma.TabIndex = 14;
+            this.button_carpma.Text = "çarpma";
+            this.button_carpma.UseVisualStyleBackColor = true;
+            this.button_carpma.Click += new System.EventHandler(this.button_carpma_Click);
+            // 
+            // textBox_carpma_sayi2
+            // 
+            this.textBox_carpma_sayi2.Location = new System.Drawing.Point(137, 228);
+            this.textBox_carpma_sayi2.Name = "textBox_carpma_sayi2";
+            this.textBox_carpma_sayi2.Size = new System.Drawing.Size(100, 23);
+            this.textBox_carpma_sayi2.TabIndex = 13;
+            // 
+            // textBox_carpma_sayi1
+            // 
+            this.textBox_carpma_sayi1.Location = new System.Drawing.Point(137, 186);
+            this.textBox_carpma_sayi1.Name = "textBox_carpma_sayi1";
+            this.textBox_carpma_sayi1.Size = new System.Drawing.Size(100, 23);
+            this.textBox_carpma_sayi1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "sayi2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "sayi1";
+            // 
+            // button_bolme
+            // 
+            this.button_bolme.Location = new System.Drawing.Point(400, 257);
+            this.button_bolme.Name = "button_bolme";
+            this.button_bolme.Size = new System.Drawing.Size(75, 23);
+            this.button_bolme.TabIndex = 19;
+            this.button_bolme.Text = "bölme";
+            this.button_bolme.UseVisualStyleBackColor = true;
+            this.button_bolme.Click += new System.EventHandler(this.button_bolme_Click);
+            // 
+            // textBox_bolme_sayi2
+            // 
+            this.textBox_bolme_sayi2.Location = new System.Drawing.Point(388, 228);
+            this.textBox_bolme_sayi2.Name = "textBox_bolme_sayi2";
+            this.textBox_bolme_sayi2.Size = new System.Drawing.Size(100, 23);
+            this.textBox_bolme_sayi2.TabIndex = 18;
+            // 
+            // textBox_bolme_sayi1
+            // 
+            this.textBox_bolme_sayi1.Location = new System.Drawing.Point(388, 186);
+            this.textBox_bolme_sayi1.Name = "textBox_bolme_sayi1";
+            this.textBox_bolme_sayi1.Size = new System.Drawing.Size(100, 23);
+            this.textBox_bolme_sayi1.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(308, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "sayi2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(308, 186);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "sayi1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_bolme);
+            this.Controls.Add(this.textBox_bolme_sayi2);
+            this.Controls.Add(this.textBox_bolme_sayi1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button_carpma);
+            this.Controls.Add(this.textBox_carpma_sayi2);
+            this.Controls.Add(this.textBox_carpma_sayi1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button_cıkart);
+            this.Controls.Add(this.textBox_cıkart_sayi2);
+            this.Controls.Add(this.textBox_cıkart_sayi1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_toplama);
+            this.Controls.Add(this.textBox_topla_sayi2);
+            this.Controls.Add(this.textBox_topla_sayi1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_topla_sayi1;
+        private System.Windows.Forms.TextBox textBox_topla_sayi2;
+        private System.Windows.Forms.Button button_toplama;
+        private System.Windows.Forms.Button button_cıkart;
+        private System.Windows.Forms.TextBox textBox_cıkart_sayi2;
+        private System.Windows.Forms.TextBox textBox_cıkart_sayi1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_carpma;
+        private System.Windows.Forms.TextBox textBox_carpma_sayi2;
+        private System.Windows.Forms.TextBox textBox_carpma_sayi1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button_bolme;
+        private System.Windows.Forms.TextBox textBox_bolme_sayi2;
+        private System.Windows.Forms.TextBox textBox_bolme_sayi1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ fonkiyon/a/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace a_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void sayilaritopla(int s1, int s2)
+        {
+            int toplam = s1 + s2;
+            MessageBox.Show(toplam.ToString());
+        }
+        
+        private void button_toplama_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox_topla_sayi1.Text);
+            int sayi2 = Convert.ToInt32(textBox_topla_sayi2.Text);
+            sayilaritopla(sayi1,sayi2);
+        }
+
+
+        void sayilaricikart(int s1, int s2)
+        {
+            int toplam = s1 - s2;
+            MessageBox.Show(toplam.ToString());
+        }
+        private void button_cıkart_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox_cıkart_sayi1.Text);
+            int sayi2 = Convert.ToInt32(textBox_cıkart_sayi2.Text);
+            sayilaricikart(sayi1, sayi2);
+        }
+
+
+        void sayilaricarp(int s1, int s2)
+        {
+            int toplam = s1 * s2;
+            MessageBox.Show(toplam.ToString());
+        }
+        private void button_carpma_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox_carpma_sayi1.Text);
+            int sayi2 = Convert.ToInt32(textBox_carpma_sayi2.Text);
+            sayilaricarp(sayi1, sayi2);
+        }
+
+        void sayilaribol(int s1, int s2)
+        {
+            int toplam = s1 / s2;
+            MessageBox.Show(toplam.ToString());
+        }
+        private void button_bolme_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox_bolme_sayi1.Text);
+            int sayi2 = Convert.ToInt32(textBox_bolme_sayi2.Text);
+            sayilaribol(sayi1, sayi2);
+        }
+    }
+}
